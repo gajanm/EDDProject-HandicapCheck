e510c9f11e1f0fac5488dfb31739e47058e075f9
Gajan Mohan Raj <gajanmohanraj@gmail.com>
2025-05-06 09:05:34 -0400

    Update edd_project_code.py

--- edd_project_code.py
+++ edd_project_code.py
@@ -253,7 +253,6 @@ while ret:
                     license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
                     _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
 
-                    cv2.imwrite(f'/content/frame_{frame_nmr}_plate.png', license_plate_crop_thresh)
 
                     # read license plate number
                     license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop)
